71e6456720fb1e9c13c8d33e7c64d31a7e98ff4e
KaranRohra <rohrakaran38@gmail.com>
2021-08-23 16:45:37 +0530

    [REF #3]: Added products in Home page

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "components/home/Banner";
+import Products from "components/products/view/Products";
 
 function Home() {
     return (
         <div>
             <Banner />
+            <Products />
         </div>
     );
 }
