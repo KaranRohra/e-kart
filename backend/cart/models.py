7587aadba74d7c2b1b97e089f07ebc088cfd2dbd
Khushboo Bajaj <68011207+khushboobajaj25@users.noreply.github.com>
2021-09-19 19:44:08 +0530

    [REF#8]:cart issues resolved

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -2,11 +2,6 @@ from accounts import models as accounts_models
 from django.db import models
 from products import models as products_models
 
-# Create your models here.
-
-
 class Cart(models.Model):
-    products = models.ManyToManyField(products_models.Product, related_name="carts", related_query_name="cart")
-    user = models.OneToOneField(
-        accounts_models.User, on_delete=models.CASCADE, related_name="carts", related_query_name="carts"
-    )
+    products = models.ManyToManyField(products_models.Product)
+    user = models.OneToOneField(accounts_models.User, on_delete=models.CASCADE)
