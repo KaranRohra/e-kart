a948c2a13bf4659be9892bb4f66f9de9ce60e4a0
Khushboo Bajaj <68011207+khushboobajaj25@users.noreply.github.com>
2021-09-20 19:05:43 +0530

    [REF #8]:cart-implemented

--- backend/cart/admin.py
+++ backend/cart/admin.py
@@ -1,9 +1,4 @@
 from cart import models
 from django.contrib import admin
 
-# Register your models here.
-
-
-@admin.register(models.Cart)
-class CartModelAdmin(admin.ModelAdmin):
-    list_display = ["user"]
+admin.site.register(models.Cart)
