8c9324067ea576ee2d45914b8c7bb7813c08d26c
anishadhameja <ani.dhameja@gmail.com>
2021-12-01 11:39:42 +0530

    [closes #17]: done with views of wishlist

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,12 @@
 from products import models
 from products import serializers
+from rest_framework import authentication
 from rest_framework import pagination
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework import views
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 
 class ProductPagination(pagination.PageNumberPagination):
@@ -13,3 +18,29 @@ class ProductAPI(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
     queryset = models.Product.objects.all()
     pagination_class = ProductPagination
+
+
+class WishlistAPI(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+
+    def post(self, request):
+        whishlist = models.WishList()
+        whishlist.user = request.user
+        whishlist.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        products = request.user.wishlist.products.all()
+        serializer = serializers.WishListSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def put(self, request):
+        product_id = request.data.get("id")
+        request.user.wishlist.product.add(product_id)
+        return Response(status=status.HTTP_200_OK, data={"message": "Product added to wishlist"})
+
+    def delete(self, request):
+        product_id = request.data.get("id")
+        request.user.wishlist.product.remove(product_id)
+        return Response(status=status.HTTP_200_OK, data={"message": "Product removed from wishlist"})
