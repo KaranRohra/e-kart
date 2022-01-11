4031fab6eff5abeb693de873d982d9d1e98b4a6f
KaranRohra <rohrakaran38@gmail.com>
2022-01-11 17:09:50 +0530

    [REF #23]: Resolved bug in address view

--- frontend/src/components/accounts/ViewAddresses.js
+++ frontend/src/components/accounts/ViewAddresses.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 import BoxSpinner from "components/common/spinners/BoxSpinner";
 import { Link } from "react-router-dom";
 import { getUserAddressAPI, updateUserAddressAPI } from "services/apis/accounts";
@@ -13,9 +13,7 @@ function ViewAddresses(props) {
             setLoading(true);
             const response = await getUserAddressAPI();
             setAddresses(response.data);
-            if (props.setSelectedAddress && response.data[0]) {
-                props.setSelectedAddress(response.data[0].id);
-            }
+            if (response.data[0] && props.setSelectedAddress) props.setSelectedAddress(response.data[0].id);
             setLoading(false);
         };
         fetchData();
@@ -46,50 +44,51 @@ function ViewAddresses(props) {
                         </Link>
                     </div>
                     <hr />
-
-                    {Object.keys(addresses).length !== 0 && (
-                        <div style={{ display: "flex" }}>
-                            {addresses.map((address, index) => (
-                                <div className="m-2" key={index}>
-                                    <Card style={{ width: "18rem", height: "15rem" }} className="text-dark">
-                                        <Card.Header style={{ display: "flex", justifyContent: "space-between" }}>
-                                            {address.name}
-                                            {props.showSelectAddressRadioButton && (
-                                                <input
-                                                    className="form-check-input"
-                                                    type="radio"
-                                                    name="selected_address"
-                                                    defaultChecked={index === 0}
-                                                    onClick={() => props.setSelectedAddress(address.id)}
-                                                />
-                                            )}
-                                        </Card.Header>
-                                        <Card.Body
-                                            style={{
-                                                display: "flex",
-                                                flexDirection: "column",
-                                                justifyContent: "space-between",
-                                            }}
-                                        >
-                                            <Card.Text>
-                                                {address.address_line_1}, <br />
-                                                {address.landmark}, {address.city}, <br />
-                                                {address.state} - {address.pincode}. <br />
-                                            </Card.Text>
-                                            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                                <Link to={`edit-address/${address.id}`} className="btn btn-primary">
-                                                    Edit
-                                                </Link>
-                                                <Button onClick={() => deleteAddress(address)} variant="danger">
-                                                    Delete
-                                                </Button>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            ))}
-                        </div>
-                    )}
+                    <div>
+                        {Object.keys(addresses).length !== 0 && (
+                            <Row>
+                                {addresses.map((address, index) => (
+                                    <Col xs={3} className="mb-2" key={index}>
+                                        <Card style={{ width: "18rem", height: "15rem" }} className="text-dark">
+                                            <Card.Header style={{ display: "flex", justifyContent: "space-between" }}>
+                                                {address.name}
+                                                {props.showSelectAddressRadioButton && (
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="radio"
+                                                        name="selected_address"
+                                                        defaultChecked={index === 0}
+                                                        onClick={() => props.setSelectedAddress(address.id)}
+                                                    />
+                                                )}
+                                            </Card.Header>
+                                            <Card.Body
+                                                style={{
+                                                    display: "flex",
+                                                    flexDirection: "column",
+                                                    justifyContent: "space-between",
+                                                }}
+                                            >
+                                                <Card.Text>
+                                                    {address.address_line_1}, <br />
+                                                    {address.landmark}, {address.city}, <br />
+                                                    {address.state} - {address.pincode}. <br />
+                                                </Card.Text>
+                                                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                                    <Link to={`edit-address/${address.id}`} className="btn btn-primary">
+                                                        Edit
+                                                    </Link>
+                                                    <Button onClick={() => deleteAddress(address)} variant="danger">
+                                                        Delete
+                                                    </Button>
+                                                </div>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        )}
+                    </div>
                 </>
             )}
         </div>
